2a0fdd085c8714b6a964d6932c6ca8e613c053cf
Rohan James <rohanjames9282@gmail.com>
2022-08-10 11:32:35 +0530

    Add nested loops to get best day to buy and sell

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,7 +2,20 @@ require 'pry-byebug'
 
 def stock_picker (stock_prices)
 
+    most_profit  = 0
+    best_day_to_buy = 0
+    best_day_to_sell = 0
 
+    for( i = 0; i < stock_prices.size; i++ )
+        for( j = i+1; j < stock_prices.size; j++ )
+            if (stock_prices[j] - stock_prices[i] > most_profit)
+                most_profit = stock_picker[j] - stock_prices[i]
+                best_day_to_buy = i
+                best_day_to_sell = j
+            end
+        end
+    end
+    best_buy_and_sell_days = [best_day_to_buy, best_day_to_sell]
 end
 
 
