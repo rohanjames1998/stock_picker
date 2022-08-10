d00faf29c88ca763b329a12151df7614300d4c25
Rohan James <rohanjames9282@gmail.com>
2022-08-10 12:01:58 +0530

    Fix if statement subtracting wrong prices

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,25 +1,24 @@
 require 'pry-byebug'
 
-def stock_picker(stock_prices)
+def stock_picker(stock_price_list)
 
     most_profit  = 0
     best_day_to_buy = 0
     best_day_to_sell = 0
 
-    stock_picker.each_with_index do |buying_day, buying_day_index|
-        stock.each_with_index do |selling_day, selling_day_index|
+    stock_price_list.each_with_index do |buying_day, buying_day_index|
+        stock_price_list.each_with_index do |selling_day, selling_day_index|
             # To avoid comparing same day or comparing days before buying day.
             if buying_day_index == selling_day_index || selling_day_index < buying_day_index
                 next
             end
-            if buying_day - selling_day > most_profit
-                most_profit = buying_day -selling_day
+            if selling_day - buying_day > most_profit
+                most_profit = selling_day - buying_day
                 best_day_to_buy = buying_day_index 
                 best_day_to_sell = selling_day_index 
             end
         end
     end
-        
     best_buy_and_sell_days = [best_day_to_buy, best_day_to_sell]
 end
 
