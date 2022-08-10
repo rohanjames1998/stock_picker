4978b9eb9ed94d9020859d3e719170512f54eaa4
Rohan James <rohanjames9282@gmail.com>
2022-08-10 11:56:28 +0530

    Remove traditional loops and add each with index loops

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,23 +1,28 @@
 require 'pry-byebug'
 
-def stock_picker (stock_prices)
+def stock_picker(stock_prices)
 
     most_profit  = 0
     best_day_to_buy = 0
     best_day_to_sell = 0
 
-    for( i = 0; i < stock_prices.size; i++ )
-        for( j = i+1; j < stock_prices.size; j++ )
-            if (stock_prices[j] - stock_prices[i] > most_profit)
-                most_profit = stock_picker[j] - stock_prices[i]
-                best_day_to_buy = i
-                best_day_to_sell = j
+    stock_picker.each_with_index do |buying_day, buying_day_index|
+        stock.each_with_index do |selling_day, selling_day_index|
+            # To avoid comparing same day or comparing days before buying day.
+            if buying_day_index == selling_day_index || selling_day_index < buying_day_index
+                next
+            end
+            if buying_day - selling_day > most_profit
+                most_profit = buying_day -selling_day
+                best_day_to_buy = buying_day_index 
+                best_day_to_sell = selling_day_index 
             end
         end
     end
+        
     best_buy_and_sell_days = [best_day_to_buy, best_day_to_sell]
 end
 
 
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
